dbf7b0ff363e9dd93a8b7581e03dd48ed87b7c94
karthik5033 <karthikkp5033@gmail.com>
2025-03-26 19:09:40 +0530

    modified the codes

--- JavaScript Programs/condition_challenges.js
+++ JavaScript Programs/condition_challenges.js
@@ -9,11 +9,46 @@
 // else{console.log("nope, num1 is not greater");
 // }
 
+// // checking if a string is equal to another string
+// let username="chai"
+// let anotherusername="chai"
+// if(username = anotherusername){
+// console.log("pick another username")
+// }
+// else{
+//     console.log("valid input");
+
+// }
+
+// // cheecking if variable is number or not :
+
+// let score = 44;
+// if (typeof score ==='number'){
+//     console.log("yep, it is a number");
+
+// }
+// else{
+//     console.log("it is not a number");
+
+// }
+
+// // checking if the boolean value is true or false
+// let isTeaReady=false
+// if(isTeaReady){
+//     console.log("tea is ready");
+
+// }
+// else{
+//     console.log("tea is not ready");
+
+// }
 
 
-// checking if a string is equal to another string
-let username="chai"
-let anotherusername="chai"
-if(username = anotherusername){
 
+// check if an array is empty or not :
+let items = [];
+if (items.length > 0) {
+  console.log("array is not empty");
+} else {
+  console.log("hell yeah !, array is empty");
 }
