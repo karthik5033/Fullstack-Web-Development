ae6a3b807bc296006377bc0da5659f8b271cb7a7
karthik5033 <karthikkp5033@gmail.com>
2025-03-26 17:00:29 +0530

    added new codes

---
+++ JavaScript Programs/primitives.js
@@ -0,0 +1,20 @@
+// number
+let balance = 120;
+let anotherBalance = new Number(120);
+
+// console.log(typeof balance);
+// console.log(anotherBalance.valueOf());
+
+// console.log(typeof balance);
+// console.log(typeof anotherBalance);
+
+let mystring="hello"
+let username="karthik"
+let oldGreet = mystring + "karthik";
+console.log(oldGreet);
+let greetMessage = `Hello ${username}`;
+console.log(greetMessage);
+
+
+
+
