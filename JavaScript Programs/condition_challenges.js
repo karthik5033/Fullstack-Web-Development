081f11e62207e3e99d589af37936cb96e835fb75
karthik5033 <karthikkp5033@gmail.com>
2025-03-26 18:52:10 +0530

    added new code

---
+++ JavaScript Programs/condition_challenges.js
@@ -0,0 +1,19 @@
+// check if the number is greater than another number
+// let num1 = 5;
+// let num2 = 8;
+// console.log("I am a regular upper code");
+
+// if (num1 < num2) {
+//   console.log("num1 is greater than num2");
+// }
+// else{console.log("nope, num1 is not greater");
+// }
+
+
+
+// checking if a string is equal to another string
+let username="chai"
+let anotherusername="chai"
+if(username = anotherusername){
+
+}
