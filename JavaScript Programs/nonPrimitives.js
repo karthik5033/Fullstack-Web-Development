e5f04c0c0dce078395ab492c937a0e5db8b42fc4
karthik5033 <karthikkp5033@gmail.com>
2025-03-26 17:22:05 +0530

    added new codes

---
+++ JavaScript Programs/nonPrimitives.js
@@ -0,0 +1,12 @@
+let username = {
+  firstname: "karthik",
+  isLoggedin: true,
+};
+
+
+console.log(username);
+console.log(typeof username);
+
+
+console.log();
+
--- JavaScript Programs/primitives.js
+++ JavaScript Programs/primitives.js
@@ -8,13 +8,20 @@ let anotherBalance = new Number(120);
 // console.log(typeof balance);
 // console.log(typeof anotherBalance);
 
-let mystring="hello"
-let username="karthik"
+let mystring = "hello";
+let username = "karthik";
 let oldGreet = mystring + "karthik";
 console.log(oldGreet);
 let greetMessage = `Hello ${username}`;
 console.log(greetMessage);
 
+let sm1 = Symbol();
+let sm2 = Symbol();
+console.log(sm1 == sm2);
+// output=false
 
 
-
+let sm3 = Symbol("abc");
+let sm4 = Symbol("abc");
+console.log(sm3 == sm4);
+// output=true
