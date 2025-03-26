64c062fa871544ae0ebadb06a04b76c9c5516030
karthik5033 <karthikkp5033@gmail.com>
2025-03-26 12:14:41 +0530

    added new codes

--- JavaScript Programs/changes.js
+++ JavaScript Programs/changes.js
@@ -1,2 +1,6 @@
 let gameName = "spiderman";
 gameName = "batman";
+console.log(gameName);
+const username = "karthik5033"
+username="mohit"
+
---
+++ JavaScript Programs/operation.js
@@ -0,0 +1,15 @@
+let addition = 4 + 5;
+let score = 3;
+let bonus = 8;
+let totalscore = score + bonus;
+console.log(addition);
+console.log(totalscore);
+let subtract = 4 + 5;
+let mult = 3 * 4;
+let divi = 6 / 3;
+let remainder = 12 % 9;
+let expo = 2 ** 4;
+let myscore=110++
+let credits=56
+credits--;
+// 
