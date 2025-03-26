9f4ae462784d9158fc2be7c32903845de5b22cfb
karthik5033 <karthikkp5033@gmail.com>
2025-03-26 11:29:36 +0530

    added new codes

---
+++ JavaScript Programs/datatype.js
@@ -0,0 +1,10 @@
+let score = 102;
+let name = "karthik5033"
+let isLoggedin = false;
+
+// object 
+let alphabets = ["a", "b", "c","d"];
+let user = {firstname:"karthik", lastname:"K P"};
+
+
+console.log(user);
