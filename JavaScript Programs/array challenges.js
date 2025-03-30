53983faef23fd9066bcfae48fb714893bb4ec38d
karthik5033 <karthikkp5033@gmail.com>
2025-03-30 10:55:31 +0530

    added new codes

---
+++ JavaScript Programs/array challenges.js
@@ -0,0 +1,50 @@
+// /* 1. Declare and array named 'teaFlavors'that contains the strings "green tea" , "black tea" , "oolong tea".
+//   Access the first element of the array and store it in a variable named 'frstTea'
+// */
+// let teaFlavors = ["green tea", "black tea", "oolong tea"];
+// let firstTea = teaFlavors[0];
+// console.log(firstTea);
+
+// /* 2.Declare an array named 'cities' containing "London", "tokyo", "paris" and "New york".
+//    Access the third element in the array and store it in a variable named 'favouriteCity'*/
+
+// let cities = ["London", "tokyo", "paris", "New york"];
+// let favouriteCity = cities[2];
+// console.log(favouriteCity);
+
+// /*3. You have and array named 'teaTypes ' containing "herbal tea", "white tea", "masala chai" ,
+//  change the second element to "Jasmine tea"*/
+//  let teaTypes=["herbal tea", "white tea", "masala chai"]
+//  teaTypes[1]="jasmine tea"
+//  console.log(teaTypes);
+
+// /* 4. Declare and array named 'citiesVisited 'containing "Mumbai"and "sydeny"
+//  Add "Berlin" to the array  using 'push ' method*/
+//  let citiesVisited =["Mumbai","sydney"]
+//  citiesVisited.push("Berlin")
+//  console.log(citiesVisited);
+
+// /*5. You have an array named 'teaOrders' with "chai","iced tea"
+// , "matcha" and "earl grey"
+// Remove the last element of the array using the 'pop'method
+// and store it in a variable named 'lastOrder'*/
+// let teaOrders = ["chai", "iced tea", "matcha", "earl grey"];
+// teaOrders.pop(3);
+// console.log(teaOrders);
+
+/* 6. You have an array named 'popularTeas' containing "green tea"
+,"oolong tea" and "chai"
+Create a soft copy of this array named 'softCopyTeas'.*/
+let popularTeas = ["green tea", "oolong tea", "chai"]; 
+let softCopyTeas=popularTeas
+console.log(softCopyTeas);
+
+
+
+/*7. You have an array named 'topCities 'containing "Berlin"
+, "Singapore", and "New York" 
+Create a hard copy of this array named 'hardCopyCities'*/
+/* */
+/* */
+/* */
+/* */
