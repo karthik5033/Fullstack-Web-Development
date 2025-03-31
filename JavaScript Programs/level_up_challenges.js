9b9aa31628fa2372e89e9775f7107359cf3b2b96
karthik5033 <karthikkp5033@gmail.com>
2025-03-31 16:06:48 +0530

    added new code

---
+++ JavaScript Programs/level_up_challenges.js
@@ -0,0 +1,17 @@
+/*1. Write a 'for' loop that loops through the array
+'["green tea","black tea","chai","oolong tea" and stops the 
+loop when it finds "chai"]' .
+store all teas before "chai" in a new array named
+'selectedTeas'*/
+
+/*2.Write a 'for' loop that loops through the array 
+'["London","New York","Paris","Berlin"]' and skips "paris"
+  Store the other cities in a new array named 'visitedCities'*/
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
