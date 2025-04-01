e69f6a5a4244fa778185ddde5cd98063e9d8e12e
karthik5033 <karthikkp5033@gmail.com>
2025-04-01 13:28:18 +0530

    modified the codes

--- JavaScript Programs/level_up_challenges.js
+++ JavaScript Programs/level_up_challenges.js
@@ -3,6 +3,18 @@
 loop when it finds "chai"]' .
 store all teas before "chai" in a new array named
 'selectedTeas'*/
+let teas = ["green tea", "black tea", "chai", "oolong tea"];
+let selectedTeas = [];
+let x;
+for (let i = 0; i < teas.length; i++) {
+  
+  const x = teas[i];
+  selectedTeas.push(x);
+  if (x == "chai") {
+    break;
+  }
+}
+console.log(selectedTeas);
 
 /*2.Write a 'for' loop that loops through the array 
 '["London","New York","Paris","Berlin"]' and skips "paris"
