fac038ec8e0f2de6a90e6454e04f79d1c3df5364
karthik5033 <karthikkp5033@gmail.com>
2025-04-01 18:14:45 +0530

    added new codes

--- JavaScript Programs/function.js
+++ JavaScript Programs/function.js
@@ -1,5 +1,5 @@
-// /*1. Write a function named 'makeTea' taht takes one 
-// parameter,'typeOfTea',and returns a string like 
+// /*1. Write a function named 'makeTea' taht takes one
+// parameter,'typeOfTea',and returns a string like
 // "making green tea"' when called with "green tea".
 // Store the result in a variable named 'teaOrder'.
 //  */
@@ -11,18 +11,45 @@
 // makeTea("green tea");
 // console.log(teaOrder);
 
-/*2. Create a function named 'orderTea' that takes one 
-parameter,'teaType'.Inside this function,create another 
-function named 'confirmOrder' taht returns a message like
-"Order confirmed for chai".
-Call 'confirmOrder' from within 'orderTea' and return the 
-result */
+// /*2. Create a function named 'orderTea' that takes one
+// parameter,'teaType'.Inside this function,create another
+// function named 'confirmOrder' taht returns a message like
+// "Order confirmed for chai".
+// Call 'confirmOrder' from within 'orderTea' and return the
+// result */
+// function orderTea(teaType) {
+//   function confirmOrder(confirm) {
+//     console.log(`Order confirmed for chai`);
+//   }
+//   return confirmOrder();
+// }
+// let orderConfirmation = orderTea();
+
 /*3. Write an arrow function named 'calculateTotal' that
 takes two parameters: 'price' and 'quantity'. the function
  should  return the total cost by multiplying the 'price'
  and 'quantity'.
  Store the result in a variable named 'totalCost'*/
-/* */
+
+const calculateTotal = (price, quantity) => {
+  return price * quantity;
+
+//   implicit return
+};
+let totalCost=calculateTotal(499,100)
+console.log(totalCost);
+
+/*4. Write a function named 'processTeaOrder'that  takes
+another function,'makeTea', as a parameter and calls it with
+the argument "earl grey"
+return teh argument of calling 'makeTea' */
+function makeTea() {
+    
+}
+function processTeaOrder(teaFunction) {
+    teaFunction('earl grey')
+}
+processTeaOrder()
 /* */
 /* */
 /* */
