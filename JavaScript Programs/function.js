957eb14ec5233de06efdfd9554e959b12d0009db
karthik5033 <karthikkp5033@gmail.com>
2025-04-01 17:15:28 +0530

    modified the code

--- JavaScript Programs/function.js
+++ JavaScript Programs/function.js
@@ -1,19 +1,30 @@
-/*1. Write a function named 'makeTea' taht takes one 
-parameter,'typeOfTea',and returns a string like 
-"making green tea"' when called with "green tea".
-Store the result in a variable named 'teaOrder'.
- */
+// /*1. Write a function named 'makeTea' taht takes one 
+// parameter,'typeOfTea',and returns a string like 
+// "making green tea"' when called with "green tea".
+// Store the result in a variable named 'teaOrder'.
+//  */
+// teaOrder = [];
+// function makeTea(typeOfTea) {
+//   console.log(`Making ${typeOfTea} `);
+//   teaOrder.push(typeOfTea);
+// }
+// makeTea("green tea");
+// console.log(teaOrder);
+
 /*2. Create a function named 'orderTea' that takes one 
 parameter,'teaType'.Inside this function,create another 
 function named 'confirmOrder' taht returns a message like
 "Order confirmed for chai".
 Call 'confirmOrder' from within 'orderTea' and return the 
 result */
+/*3. Write an arrow function named 'calculateTotal' that
+takes two parameters: 'price' and 'quantity'. the function
+ should  return the total cost by multiplying the 'price'
+ and 'quantity'.
+ Store the result in a variable named 'totalCost'*/
 /* */
 /* */
 /* */
 /* */
 /* */
 /* */
-/* */
-
