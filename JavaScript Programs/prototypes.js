476bf22163fe8603ed13780e70c85bdca9e9fc9d
karthik5033 <karthikkp5033@gmail.com>
2025-04-02 19:11:18 +0530

    added a new code

--- JavaScript Programs/prototypes.js
+++ JavaScript Programs/prototypes.js
@@ -1,18 +1,24 @@
-const { prototype } = require("postcss/lib/previous-map");
+// const { prototype } = require("postcss/lib/previous-map");
 
-let computer = {
-  cpu: 12,
-};
-let lenovo = { screen: "HD", __proto__: computer };
+// let computer = {
+//   cpu: 12,
+// };
+// let lenovo = { screen: "HD", __proto__: computer };
+// // let tomHardware = {};
+// console.log(`computer`, computer.__proto__);
+
 // let tomHardware = {};
-console.log(`computer`, computer.__proto__);
+// console.log(`lenovo`, lenovo.__proto__);
+// let genericCar = { tyres: 4 };
+// let tesla={
+//     driver:"AI",
+// }
+// Object.setPrototypeOf(tesla,genericCar)
+// console.log(`tesla`, Object.getPrototypeOf(tesla));
 
-let tomHardware = {};
-console.log(`lenovo`, lenovo.__proto__);
-let genericCar = { tyres: 4 };
-let tesla={
-    driver:"AI",
-}
-Object.setPrototypeOf(tesla,genericCar)
-console.log(`tesla`, tesla.tyres);
-
+let person = {
+  greet: function () {
+    console.log("hello world");
+  },
+};
+person.greet();
