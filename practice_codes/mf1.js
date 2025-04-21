f92f894e66f5abdd115e8dfa4a90599dcf9a1626
karthik5033 <karthikkp5033@gmail.com>
2025-04-21 15:21:52 +0530

    just a normal day and a normal code

---
+++ practice_codes/mf1.js
@@ -0,0 +1,35 @@
+// class Employee {
+//   constructor(name, age, salery) {
+//     this.name = name;
+//     this.age = age;
+//     this.salery = salery;
+//     this.details = function () {
+//       return `the employee ${this.name} is of age ${this.age} and has a salery of ${this.salery}`;
+//     };
+//   }
+// }
+
+// const p1=new Employee("mf",20,10)
+// console.log(p1.details())
+
+// class Employee {
+//     constructor(name, age, salery) {
+//       this.name = name;
+//       this.age = age;
+//       this.salery = salery;}
+//       details() {
+//         return `the employee ${this.name} is of age ${this.age} and has a salery of ${this.salery}`;
+//       };
+    
+//   }
+  
+//   const p1=new Employee("mf",20,10)
+//   console.log(p1.details())
+  
+
+class area {
+    constructor(length,bredth){
+        this.length=length;
+        this.bredth=bredth;
+    }
+}
