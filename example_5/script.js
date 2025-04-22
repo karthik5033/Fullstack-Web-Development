d4c482ab4c2112e007677daf4a0e0263f994e1e2
karthik5033 <karthikkp5033@gmail.com>
2025-04-22 14:52:02 +0530

    updated the code

--- example_5/script.js
+++ example_5/script.js
@@ -1,1 +1,7 @@
-alert("connected")
+document
+  .getElementById("changeTextButton")
+  .addEventListener("click", function () {
+    let paragraph = document.getElementById("myParagraph");
+    console.log(paragraph);
+    paragraph.textContent="this paragraph is changed";
+  });
