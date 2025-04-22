5f03d6c895528ab8f0523acefc3f79a9cdbc1ad7
karthik5033 <karthikkp5033@gmail.com>
2025-04-22 15:22:00 +0530

    updated the code

--- example_5/script.js
+++ example_5/script.js
@@ -1,7 +1,18 @@
+// Example 1
+
 document
   .getElementById("changeTextButton")
   .addEventListener("click", function () {
     let paragraph = document.getElementById("myParagraph");
     console.log(paragraph);
-    paragraph.textContent="this paragraph is changed";
+    paragraph.textContent = "this paragraph is changed";
+  });
+
+// Example 2
+
+document
+  .getElementById("highlightFirstCity")
+  .addEventListener("click", function () {
+    let citieslist = document.getElementById("citiesList");
+    cities
   });
