f93a61e5ab1d4210c5d855c9c18e9d3d8aa4783e
karthik5033 <karthikkp5033@gmail.com>
2025-05-02 19:07:32 +0530

    updated

---
+++ JavaScript Programs/promises.js
@@ -0,0 +1,15 @@
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let success = true;
+      if (success) {
+        resolve("data fetched successfully");
+      } else {
+        reject("Error fetching data");
+      }
+    }, 3000);
+  });
+}
+
+let response = fetchData();
+console.log(response);
