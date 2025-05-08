a153cc52f60c28dd009b76f4575f48c5add093d9
karthik5033 <karthikkp5033@gmail.com>
2025-05-08 13:29:58 +0530

    removed the code

--- JavaScript Programs/jj.js
+++ JavaScript Programs/jj.js
@@ -8,10 +8,9 @@ class Employee {
 p1= new Employee("karthik")
 console.log(p1.name);
 console.log(p1.name);
-console.log("this is a random code");
-console.log("this is a random code");
 
-l
+
+
 
 
 
