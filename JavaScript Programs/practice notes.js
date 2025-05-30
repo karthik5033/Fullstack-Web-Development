cd53835655dad5d2fcaff253bfabc5c168779691
karthik5033 <karthikkp5033@gmail.com>
2025-05-30 18:23:42 +0530

    a

--- JavaScript Programs/practice notes.js
+++ JavaScript Programs/practice notes.js
@@ -1,8 +1,31 @@
-function Person() {
-  this.name = this.name;
-  this.sayHello = function () {
-    console.log("hi, im" + this.name);
-  };
-  const p1 =new Person("abc");
-  abc.sayHello();
-}
+// function Person() {
+//   this.name = this.name;
+//   this.sayHello = function () {
+//     console.log("hi, im" + this.name);
+//   };
+//   const p1 =new Person("abc");
+//   abc.sayHello();
+// }
+
+// // closures
+// function outer() {
+//   let name = "abc";
+//   function inner() {
+//     console.log("hello " + name);
+//   }
+//   return inner;
+// }
+// let greet = outer();
+// greet();
+
+// function outer(){
+
+// }
+
+let mc = function(name, age) {
+ 
+  return ` the name is ${name} and age is ${age}`;
+};
+
+let a = mc("de", 4);
+console.log(a);
