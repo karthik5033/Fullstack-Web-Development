b0c1fed5255858d1028af82c95d442a0f294ea88
karthik5033 <karthikkp5033@gmail.com>
2025-08-26 20:09:02 +0530

    a

--- example_4/script.js
+++ example_4/script.js
@@ -1,2 +1,10 @@
-console.log(document);
-console.log(document.getElementsByTagName("h1"));
+document.getElementById("mf")
+.addEventListener("mouseover", function () {
+  const paragraph = document.getElementById("mf");
+
+  paragraph.textContent = "text changed";
+})
+.addEventListener("mouseleave",function(){
+const paragraph = document.getElementById("mf");
+paragraph.textContent = "hello mf";
+})
