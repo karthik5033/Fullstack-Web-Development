cd3de98e7b12d3426552dfc1105b7044a975a00e
karthik5033 <karthikkp5033@gmail.com>
2025-08-26 23:36:02 +0530

    added some dom practiced codes

--- example_4/script.js
+++ example_4/script.js
@@ -1,10 +1,77 @@
-document.getElementById("mf")
-.addEventListener("mouseover", function () {
-  const paragraph = document.getElementById("mf");
+// // changing text while hovering(incorrect way)
+// document.getElementById("mf")
+// .addEventListener("mouseover", function () {
+//   const paragraph = document.getElementById("mf");
+//   paragraph.textContent = "text changed";
+// })
+// .addEventListener("mouseleave",function(){
+// const paragraph = document.getElementById("mf");
+// paragraph.textContent = "hello mf";
+// })
 
-  paragraph.textContent = "text changed";
-})
-.addEventListener("mouseleave",function(){
-const paragraph = document.getElementById("mf");
-paragraph.textContent = "hello mf";
-})
+
+// // Correct way of doing
+// let paragraph=document.getElementById("mf");
+
+// paragraph.addEventListener("mouseover",function(){
+//   paragraph.textContent="the text has changed";
+//   paragraph.style.color="red";
+// })
+// paragraph.addEventListener("mouseleave",function(){
+//   paragraph.textContent="hello mf";
+//   paragraph.style.color = "";
+// })
+
+
+// // changing color while hovering
+// let paragraph=document.getElementById("mf");
+
+// paragraph.addEventListener("mouseover",function(){
+//   paragraph.textContent="the text has changed";
+//   paragraph.style.color="red";
+// })
+// paragraph.addEventListener("mouseleave",function(){
+//   paragraph.textContent="hello mf";
+//   paragraph.style.color = "";
+// })
+
+// // changing color while clicking(incorrect method)
+// let x= document.getElementById("aa");
+// x.addEventListener("click",function() {
+//   document.getElementById("a").style.color="green"
+// })
+// y=document.getElementById("aa");
+// y.addEventListener("click", function () {
+//   document.getElementById("a").style.color = "black";
+// });
+
+
+// // correct way of clicking twice and changing colors alternatively
+// let x=document.getElementById("aa");
+// let y = document.getElementById("a");
+// let isGreen=false;
+// x.addEventListener("click",function(){
+// if (isGreen) {
+//   y.style.color = "black";
+// } 
+// else
+// { y.style.color = "green";} 
+//   isGreen=!isGreen;
+//   // toggles between true and false in every loop
+// }
+// )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
