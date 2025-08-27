450bde0332a758b19e62c511c3fd0870d5c3c608
karthik5033 <karthikkp5033@gmail.com>
2025-08-28 00:06:45 +0530

    a

--- JavaScript Programs/closures.js
+++ JavaScript Programs/closures.js
@@ -1,13 +1,23 @@
+// function outer() {
+//   let counter = 4;
+//   return function () {
+//     counter++;
+//     return counter;
+//   };
+// }
+
+// let increment = outer();
+// console.log(increment());
+// console.log(increment());
+// console.log(increment());
+// console.log(increment());
+
 function outer() {
-  let counter = 4;
-  return function () {
-    counter++;
-    return counter;
-  };
+  let name = "karthik";
+  function inner() {
+    console.log(name);
+  }
+  
 }
 
-let increment = outer();
-console.log(increment());
-console.log(increment());
-console.log(increment());
-console.log(increment());
+inner();
--- example_5/script.js
+++ example_5/script.js
@@ -26,3 +26,32 @@ addEventListener("click",function () {
  order.style.padding="5px"
   
 })
+// Example 4
+document.getElementById("addNewItem")
+.addEventListener("click",function(){
+  
+  let newItem= document.createElement("li")
+  newItem.textContent="eggs"
+  x=document.getElementById("shoppingList");
+  x.appendChild(newItem);
+})
+
+// example 5
+document.getElementById("removeLastTask")
+.addEventListener("click", function(){
+let x= document.getElementById("taskList");
+x.lastElementChild.remove()
+})
+
+// Example 6
+document.getElementById("clickMeButton")
+.addEventListener("click",function(){
+  alert("shut up");
+})
+
+// Example 7
+document.getElementById("teaList")
+.addEventListener("click",function(event){
+  console.log(event.target);
+  
+})
