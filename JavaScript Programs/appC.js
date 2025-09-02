dcc3e7add037b1ab52649b66f5f5b80fea9e1511
karthik5033 <karthikkp5033@gmail.com>
2025-09-02 19:15:34 +0530

    a

---
+++ JavaScript Programs/appC.js
@@ -0,0 +1,2 @@
+let math=require("./commonJS.js")
+console.log(math.add(2,4));
---
+++ JavaScript Programs/appM.js
@@ -0,0 +1,8 @@
+// default import 
+import multiply from "./Mathoperation.js";
+// named import 
+import { add, subtract } from "./Mathoperation.js";
+console.log(multiply(2,4));
+console.log(add(7,5));
+console.log(subtract(7, 5));
+
