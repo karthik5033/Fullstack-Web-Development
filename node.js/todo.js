e32099674e21af4453d3eb2605357497f3032102
karthik5033 <karthikkp5033@gmail.com>
2025-09-04 00:19:19 +0530

    a

---
+++ node.js/todo.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const filePath = "./tasks.json";
+const loadTasks = () => {
+  try {
+    const dataBuffer = fs.readFileSync(filePath);
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (error) {
+    return [];
+  }
+};
+
+const saveTask = (tasks) => {
+  let dataJSON = JSON.stringify(tasks);
+  fs.writeFileSync(filePath, dataJSON);
+};
+
+const addTask = (task) => {
+  const tasks = loadTasks();
+  tasks.push({ task });
+  saveTask(tasks);
+  console.log("task added", task);
+};
+
+const listTasks = () => {
+  let tasks = loadTasks();
+  tasks.forEach((task, index) => console.log(`${index + 1}-${task.task}`));
+};
+const command = process.argv[2];
+const argument = process.argv[3];
+
+if (command === "add") {
+  addTask(argument);
+} else if (command === "list") {
+  listTasks();
+} else if (command === "remove") {
+  removeTask(parseInt(argument));
+} else {
+  console.log("command not found !");
+}
