60bfb52702fb3785eef3ac1ee4b45fa488bd09fb
karthik5033 <karthikkp5033@gmail.com>
2025-09-08 00:46:43 +0530

    a

---
+++ node.js/server/server.js
@@ -0,0 +1,37 @@
+const { error } = require("console");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const port = 3000;
+const server = http.createServer((req, res) => {
+  const filePath = path.join(
+    __dirname,
+    req.url === "/" ? "index.html" : "req.url"
+  );
+  console.log(filePath);
+  
+  let extname = String(path.extname(filePath)).toLowerCase();
+  const mimeTypes = {
+    // what types of file is being supported
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".png": "text/png",
+  };
+  const contentType = mimeTypes[extname] || "application/octet-stream";
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        res.writeHead(404, { "Content-Type": text / html });
+        res.end("404:stop watching such stuffs");
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf-8");
+    }
+  });
+});
+server.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
+});
